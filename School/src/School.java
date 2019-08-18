9ddecc1aa173b4c32e525763c4547d2ff524aedb
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-18 17:18:51 -0400

    Commit

---
+++ School/src/School.java
@@ -0,0 +1,71 @@
+
+public class School {
+
+Student head;
+
+public static void main (String[] args) {
+	
+	School d = new School(); 
+	d.addFreshman("h",9);
+	
+}
+
+public void addFreshman(String name, int grade) {
+	Student freshman = new Student (name,grade);
+	
+	if(freshman.grade != 9) {
+		System.out.println("Student can not be added since he is a freshman");
+		}else {
+	
+	freshman.next = head;
+	
+	head = freshman;
+	
+	System.out.print(freshman.name + " has been added");
+	
+	}
+	
+	
+	
+}
+	
+public void removeStudent(int position) {
+	
+	if(head == null) {
+		return;
+	}
+	
+	Student temp = head;
+	
+	if(position == 0 ) {
+		
+		head  = temp.next;
+		return;
+		
+		
+	}
+	
+	
+	for(int a=0; temp != null &&  a<position-1;a++)
+		temp = temp.next;
+	
+	if(temp==null|| temp.next==null)
+		return;
+				
+				Student next = temp.next.next;
+				
+				temp.next = next;
+	
+}
+	
+public void printList() 
+{ 
+    Student tnode = head; 
+    while (tnode != null) 
+    { 
+        System.out.print(tnode.grade+" "); 
+        tnode = tnode.next; 
+    } 
+} 
+	
+	}
---
+++ School/src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+String name;
+int grade;
+Student next;
+	
+	Student(String name, int grade){
+		this.name = name;
+		this.grade= grade;
+		next = null;
+	}
+	
+	
+	
+}
