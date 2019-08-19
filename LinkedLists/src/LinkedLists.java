eb6a6e1010422b3ef36bac0db91f9a9482e02edb
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-18 23:43:13 -0400

    Commit

---
+++ LinkedLists/src/LinkedLists.java
@@ -0,0 +1,40 @@
+
+public class LinkedLists {
+ 
+                public int findNode (Node node, int x){
+                                int count=1;
+                                /* keeps on going till the next node
+                                   is null (basically after the last node)*/
+                                while(node.next != null) {
+               
+                                count++;
+                                                if(count == x) {  //starts count after kth element is reached
+                                                                x++;
+                                                }
+                                }
+                               
+                               
+                               
+                                return x;
+                               
+                               
+                               
+                               
+                }
+               
+               
+ 
+ 
+class Node { //node class
+                Node next;
+   int data;
+               
+                public Node(int data) {
+                                this.data = data;
+                }
+ 
+ 
+               
+}
+ 
+}
