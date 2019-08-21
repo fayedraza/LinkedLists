c7608eac9cd70b727dd0f8531af13aea60d916e0
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-21 14:41:59 -0400

    Commit

--- LinkedLists/src/LinkedLists.java
+++ LinkedLists/src/LinkedLists.java
@@ -2,26 +2,27 @@
 //purpose is to introduce linkedlists which is a data structure
 public class LinkedLists {
  
-                public int findNode (Node node, int x){
-                                int count=1;
-                                /* keeps on going till the next node
-                                   is null (basically after the last node)*/
-                                while(node.next != null) {
-               
-                                count++;
-                                                if(count == x) {  //starts count after kth element is reached
-                                                                x++;
-                                                }
-                                }
-                               
-                               
-                               
-                                return x;
-                               
-                               
-                               
-                               
-                }
+	public int findNode (Node node, int x){
+        int count=1;
+        /* keeps on going till the next node
+           is null (basically after the last node)*/
+        while(node.next != null) {
+
+        count++;
+          if(count == x) {  //starts count after kth element is reached
+                     x++;
+               }
+        }
+       
+       
+       
+        return x;
+       
+       
+       
+       
+}
+                
                
                
  
@@ -31,7 +32,7 @@ class Node { //node class
 	Node next;
    int data;
                
-                public Node(int data) {//parameter
+                public Node(int data) {//constructor with int parameter
                     this.data = data;
                 }
  
